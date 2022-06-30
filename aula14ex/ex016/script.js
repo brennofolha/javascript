02602ee94412e0c893c118e8abf4e449a25fbb78
Brenno M. Folha <105752384+brennofolha@users.noreply.github.com>
2022-06-29 23:21:34 -0400

    criando um exercicio

---
+++ aula14ex/ex016/script.js
@@ -0,0 +1,8 @@
+function contar() {
+    var n1 = Number(document.getElementById('Início'))
+    var n2 = Number(document.getElementById('Fim'))
+    var n3 = Number(document.getElementById('Passo'))
+    for (var n1; n1 <= n2; ;) {
+        
+    }
+}
