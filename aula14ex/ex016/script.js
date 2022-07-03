75208f5728d0bb89e80f90b6da490001f807fefa
Brenno M. Folha <105752384+brennofolha@users.noreply.github.com>
2022-07-03 10:29:37 -0400

    .

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -1,8 +1,28 @@
 function contar() {
-    var n1 = Number(document.getElementById('Início'))
-    var n2 = Number(document.getElementById('Fim'))
-    var n3 = Number(document.getElementById('Passo'))
-    for (var n1; n1 <= n2; ;) {
+    let ini = document.getElementById('txti')
+    let fim = document.getElementById('txtf')
+    let passo = document.getElementById('txtp')
+    let res = document.getElementById('res')
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
+        alert('[ERRO] Faltam dados!')
+
+    } else{
+        res.innerHTML = 'Contando: '
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value) 
+        // contagem crescente
+        if (i < f){
+            for (let c = i; c <= f; c += p){
+            res.innerHTML += ` ${c} 👉`
+        }
+        // contagem regressiva
+        } else {
+           for (let c = i; c >= f; c-= p){
+            res.innerHTML += ` ${c} 👉`
+           }
+        }
         
+        res.innerHTML += `🏁`
     }
 }
