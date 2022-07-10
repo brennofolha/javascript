0e043244f7955ec1edef47de3a881079aef08ae9
Brenno M. Folha <105752384+brennofolha@users.noreply.github.com>
2022-07-09 23:07:57 -0400

    Exercícios JavaScript (Parte 5) - Curso JavaScript

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -4,13 +4,19 @@ function contar() {
     let passo = document.getElementById('txtp')
     let res = document.getElementById('res')
     if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
-        alert('[ERRO] Faltam dados!')
+        res.innerHTML = 'impossível contar!'
+        //alert('[ERRO] Faltam dados!')
+        
 
     } else{
-        res.innerHTML = 'Contando: '
+        res.innerHTML = 'Contando: <br>'
         let i = Number(ini.value)
         let f = Number(fim.value)
         let p = Number(passo.value) 
+        if (p <= 0) {
+            alert('Passo inválido! Considerando PASSO 1')
+            p = 1
+        }
         // contagem crescente
         if (i < f){
             for (let c = i; c <= f; c += p){
