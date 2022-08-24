b99934e13eb83f9261c33bcf5a8f2d9a9d27f83c
Brenno M. Folha <105752384+brennofolha@users.noreply.github.com>
2022-08-24 16:12:32 -0400

    Update script.js

---
+++ aula16ex/script.js
@@ -0,0 +1,21 @@
+let num = document.querySelector('input#fnum')
+let lista = document.querySelector('select#flista')
+let res = document.querySelector('div#res')
+let valores = []
+function isnumero (n) {
+    if(Number(n) >= 1 && Number(n) <= 100) {
+        return true
+    } else {
+        return false
+    }
+}
+function inlista(n, l) {
+    
+}
+function adicionar() {
+    if(isnumero(num.value) && !inlista(num.value, valores)){
+
+    } else{
+        window.alert('Valor inválido ou já encontrado na lista')
+    }
+}
